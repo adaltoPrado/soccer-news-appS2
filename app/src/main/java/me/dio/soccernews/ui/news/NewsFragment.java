f4fd054aa57861a1d30412c9c5a9089f9c1cb1f6
Adalto Prado <adaltoprado@proton.me>
2022-07-24 18:46:19 -0300

    Inclusão do 'Room', funcionalidades de 'Share' e 'Favorite'.

--- app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
@@ -1,6 +1,8 @@
 package me.dio.soccernews.ui.news;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,15 +10,20 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.room.Room;
 
+import me.dio.soccernews.MainActivity;
+import me.dio.soccernews.data.local.AppDatabase;
 import me.dio.soccernews.databinding.FragmentNewsBinding;
 import me.dio.soccernews.ui.adapters.NewsAdapter;
 
 public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
+    private AppDatabase db;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         NewsViewModel newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
@@ -26,7 +33,27 @@ public class NewsFragment extends Fragment {
 
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
-            binding.rvNews.setAdapter(new NewsAdapter(news));
+            binding.rvNews.setAdapter(new NewsAdapter(news, updatedNews -> {
+                MainActivity activity = (MainActivity) getActivity();
+                if (activity != null) {
+                    activity.getDb().newsDao().save(updatedNews);
+                }
+            }));
+        });
+
+        newsViewModel.getState().observe(getViewLifecycleOwner(), state -> {
+            switch (state){
+                case DOING:
+                    //TODO Iniciar SwipeRefreshlayout (loading).
+                    break;
+                case DONE:
+                    //TODO Finalizar SwipeRefreshlayout (loading).
+                    break;
+                case ERROR:
+                    //TODO Finalizar SwipeRefreshlayout (loading).
+                    //TODO Mostrar um erro genérico.
+
+            }
         });
         return root;
     }
--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,12 +1,16 @@
 package me.dio.soccernews.ui.news;
 
+import android.app.Application;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.room.Room;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.local.AppDatabase;
 import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.domain.News;
 import retrofit2.Call;
@@ -17,7 +21,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
+    public  enum State{
+        DOING, DONE, ERROR;
+    }
+
     private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+
+    private final MutableLiveData<State> state = new MutableLiveData<>();
+
     private final SoccerNewsApi api;
 
     public NewsViewModel() {
@@ -30,19 +41,22 @@ public class NewsViewModel extends ViewModel {
     }
 
     private void findNews() {
+        state.setValue(State.DOING);
         api.getNews().enqueue(new Callback<List<News>>() {
             @Override
             public void onResponse(Call<List<News>> call, Response<List<News>> response) {
                 if (response.isSuccessful()){
                     news.setValue(response.body());
+                    state.setValue(State.DONE);
+
                 }else {
-                    //TODO Pensar em uma estratégia de tratamento de erros.
+                    state.setValue(State.ERROR);
                 }
             }
 
             @Override
             public void onFailure(Call<List<News>> call, Throwable t) {
-                //TODO Pensar em uma estratégia de tratamento de erros.
+                state.setValue(State.DOING);
             }
         });
     }
@@ -50,4 +64,8 @@ public class NewsViewModel extends ViewModel {
     public LiveData<List<News>> getNews() {
         return this.news;
     }
+    public LiveData<State> getState() {
+        return this.state;
+    }
+
 }
